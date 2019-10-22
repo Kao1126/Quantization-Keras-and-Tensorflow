f6e38b86ca2c3a89c938916b93edb30677c76165
Kao1126 <53559678+Kao1126@users.noreply.github.com>
2019-10-23 00:02:33 +0800

    Update frozen.py

--- frozen.py
+++ frozen.py
@@ -28,6 +28,10 @@ def frozen(checkpoint_path, model_path, frozen_path):
         saver = tf.compat.v1.train.Saver()  
         saver.restore(eval_sess, checkpoint_path + '/checkpoints')
 
+        
+        ''' fix error if you have normalization layer'''
+        fix_error()
+        
         frozen_graph_def = tf.compat.v1.graph_util.convert_variables_to_constants(
             eval_sess,
             eval_graph_def,
@@ -39,3 +43,27 @@ def frozen(checkpoint_path, model_path, frozen_path):
         with open(frozen_path, 'wb') as f:
             f.write(frozen_graph_def.SerializeToString())
 
+            
+def fix_error():
+    gp = tf.get_default_graph().as_graph_def()
+    for node in gp.node:
+        if node.op == 'RefSwitch':
+            node.op = 'Switch'
+            for index in range(len(node.input)):
+                if 'moving_' in node.input[index]:
+                    node.input[index] = node.input[index] + '/read'
+        elif node.op == 'AssignSub':
+            node.op = 'Sub'
+            if 'use_locking' in node.attr: del node.attr['use_locking']
+        elif node.op == 'AssignAdd':
+            node.op = 'Add'
+            if 'use_locking' in node.attr: del node.attr['use_locking']
+        elif node.op == 'Assign':
+            node.op = 'Identity'
+            if 'use_locking' in node.attr: del node.attr['use_locking']
+            if 'validate_shape' in node.attr: del node.attr['validate_shape']
+            if len(node.input) == 2:
+                # input0: ref: Should be from a Variable node. May be uninitialized.
+                # input1: value: The value to be assigned to the variable.
+                node.input[0] = node.input[1]
+                del node.input[1]            
