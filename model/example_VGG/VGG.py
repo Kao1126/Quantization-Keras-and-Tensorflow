b97217a327c0b5c33ce1e2a5c6acbefe6a549ab4
Kao1126 <53559678+Kao1126@users.noreply.github.com>
2019-10-23 00:00:39 +0800

    Update VGG.py

--- model/example_VGG/VGG.py
+++ model/example_VGG/VGG.py
@@ -9,28 +9,26 @@ from tensorflow.keras.applications import VGG16
 from tensorflow.keras.utils import plot_model
 
 
-class create_model(object):
+def model_(input_shape, classes):
 
+    model = VGG16(include_top=False, input_shape=input_shape)
+    x = Flatten()(model.output)
+    x = Dense(1024, activation='relu', name='ft1')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1024, activation='relu', name='ft2')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(classes, activation='softmax')(x)
+    
+    custom_model = Model(inputs=model.input, outputs=x)
+    custom_model.summary()
 
-    def model_(self, input_shape, classes):
+    custom_model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy']) 
+    
+    os.environ["HDF5_USE_FILE_LOCKING"] = 'FALSE'
+    custom_model.save('VGG.h5')
 
-        model = VGG16(weights='imagenet', include_top=False, input_shape=input_shape)
-        x = Flatten()(model.output)
-        x = Dense(1024, activation='relu', name='ft1')(x)
-        x = Dropout(0.5)(x)
-        x = Dense(1024, activation='relu', name='ft2')(x)
-        x = Dropout(0.5)(x)
-        x = Dense(classes, activation='softmax')(x)
-        custom_model = Model(inputs=model.input, outputs=x)
-
-        custom_model.summary()
-
-        custom_model.compile(
-            loss='categorical_crossentropy',
-            optimizer='adam',
-            metrics=['accuracy']) 
-
-        os.environ["HDF5_USE_FILE_LOCKING"] = 'FALSE'
-        custom_model.save(model_path)
-
-        return custom_model
+if  __name__ == "__main__":
+    model_((250, 250, 3), 3)
